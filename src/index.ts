fd1070099bb9c907d44abc421e51f74cee1b9989
Vyachslav Demot <v.demot@gmail.com>
2018-11-08 13:44:05 +0300

    Linting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 export default class Base64ToFile
 {
+	// Возвращает расширение файла по его имени с расширением
+	public static fileExtension(fileName: string): string
+	{
+		const match = fileName.match(/[^.][aA-zZ0-9]+$/i);
+
+		return !match ? '' : match[0].toLowerCase();
+	}
+
 	private view: typeof window;
-
-	readonly saveLink: HTMLAnchorElement;
-	readonly canUseSaveLink: boolean;
-	readonly forceSaveableType: string;
-
+	private saveLink: HTMLAnchorElement;
+	private canUseSaveLink: boolean;
+	private forceSaveableType: string;
 	private blob: Blob;
+
 	private name: string;
 
 	constructor(base64: string, name: string)
@@ -16,11 +23,11 @@ export default class Base64ToFile
 		this.canUseSaveLink = 'download' in this.saveLink;
 		this.forceSaveableType = this.extensionToMime();
 
-		let ext = Base64ToFile.fileExtension(name);
+		const ext = Base64ToFile.fileExtension(name);
 		let type = this.extensionToMime(ext);
 
 		if ( !type ) {
-			console.warn('Unknown file format, use "%s"', this.forceSaveableType);
+			// console.warn('Unknown file format, use "%s"', this.forceSaveableType);
 			type = this.forceSaveableType;
 		}
 
@@ -33,15 +40,54 @@ export default class Base64ToFile
 		return this.view.URL;
 	}
 
+	public save(): void
+	{
+		const blob = this.autoBom();
+
+		const objectUrl = this.url.createObjectURL(blob);
+
+		if ( this.canUseSaveLink ) {
+			window.setTimeout(() => {
+				this.saveLink.href = objectUrl;
+				this.saveLink.download = this.name;
+				this.click(this.saveLink);
+			});
+
+			return;
+		}
+
+		if ( FileReader ) {
+			const reader = new FileReader();
+
+			reader.onloadend = () => {
+				const url = reader.result as string;
+				const popup = this.view.open(url, '_blank');
+				if (!popup) {
+					this.view.location.href = url;
+				}
+			};
+
+			reader.readAsDataURL(blob);
+
+			return;
+		}
+
+		if (blob.type === this.forceSaveableType) {
+			this.view.location.href = objectUrl;
+		} else if ( !this.view.open(objectUrl, '_blank') ) {
+			this.view.location.href = objectUrl;
+		}
+	}
+
 	private click(node: HTMLElement): void
 	{
-		let event = new MouseEvent('click');
+		const event = new MouseEvent('click');
 		node.dispatchEvent(event);
 	}
 
 	private autoBom(): Blob
 	{
-		let blob = this.blob;
+		const blob = this.blob;
 
 		if ( /^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(blob.type) ) {
 			return new Blob([String.fromCharCode(0xFEFF), blob], { type: blob.type });
@@ -53,20 +99,20 @@ export default class Base64ToFile
 	// Конвертация base64 строки в Blob объект
 	private base64toBlob(base64: string, contentType: string): Blob
 	{
-		let sliceSize = 512;
+		const sliceSize = 512;
 
-		let byteCharacters = atob(base64);
-		let byteArrays = [];
+		const byteCharacters = atob(base64);
+		const byteArrays = [];
 
 		for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-			let slice = byteCharacters.slice(offset, offset + sliceSize);
+			const slice = byteCharacters.slice(offset, offset + sliceSize);
 
-			let byteNumbers = new Array(slice.length);
+			const byteNumbers = new Array(slice.length);
 			for (let i = 0; i < slice.length; i++) {
 				byteNumbers[i] = slice.charCodeAt(i);
 			}
 
-			let byteArray = new Uint8Array(byteNumbers);
+			const byteArray = new Uint8Array(byteNumbers);
 
 			byteArrays.push(byteArray);
 		}
@@ -94,53 +140,6 @@ export default class Base64ToFile
 				return 'application/pdf';
 			default:
 				return '';
-		}
-	}
-
-	// Возвращает расширение файла по его имени с расширением
-	public static fileExtension(fileName: string): string
-	{
-		let match = fileName.match(/[^.][aA-zZ0-9]+$/i);
-
-		return !match ? '' : match[0].toLowerCase();
-	}
-
-	public save(): void
-	{
-		let blob = this.autoBom();
-
-		let objectUrl = this.url.createObjectURL(blob);
-
-		if ( this.canUseSaveLink ) {
-			window.setTimeout(() => {
-				this.saveLink.href = objectUrl;
-				this.saveLink.download = this.name;
-				this.click(this.saveLink);
-			});
-
-			return;
-		}
-
-		if ( FileReader ) {
-			let reader = new FileReader();
-
-			reader.onloadend = () => {
-				let url = reader.result as string;
-				let popup = this.view.open(url, '_blank');
-				if (!popup) {
-					this.view.location.href = url;
-				}
-			};
-
-			reader.readAsDataURL(blob);
-
-			return;
-		}
-
-		if (blob.type === this.forceSaveableType) {
-			this.view.location.href = objectUrl;
-		} else if ( !this.view.open(objectUrl, '_blank') ) {
-			this.view.location.href = objectUrl;
 		}
 	}
 }
