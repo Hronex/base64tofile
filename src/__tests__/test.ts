6cc225350384e2a0ba6ab6fc98f725272bb71054
Vyacheslav Demot <v.demot@gmail.com>
2020-09-21 15:10:25 +0300

    Added tests

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -6,3 +6,22 @@ test('Base64ToFile', (done) => {
 	expect(f).toBeInstanceOf(Base64ToFile);
 	done();
 });
+
+describe('Get file extension', () => {
+	it(`example.jpg must to be jpg`, done => {
+		expect(Base64ToFile.fileExtension('example.jpg')).toBe('jpg');
+		done();
+	});
+	it(`example.test.png must to be png`, done => {
+		expect(Base64ToFile.fileExtension('example.test.png')).toBe('png');
+		done();
+	});
+	it(`example.mp4 must to be mp4`, done => {
+		expect(Base64ToFile.fileExtension('example.mp4')).toBe('mp4');
+		done();
+	});
+	it(`.gitignore must to be gitignore`, done => {
+		expect(Base64ToFile.fileExtension('.gitignore')).toBe('gitignore');
+		done();
+	});
+});
